587ade545c5352b6609154b3b1a5c56e3f486f81
shamilv05 <vagabov.sha@phystech.edu>
2019-11-20 23:45:25 +0300

    constructors for node, skiplist added; coin flip function added

--- source/skiplist.cpp
+++ source/skiplist.cpp
@@ -1,5 +1,25 @@
-//
-// Created by Shamil on 2019-11-19.
-//
-
 #include "skiplist.hpp"
+
+Node::Node(int key, uint64_t level) {
+    this->key = key;
+    this->forward = new Node*[level + 1];
+    std::memset(forward, 0, sizeof(Node *[level + 1]));
+}
+
+SkipList::SkipList(uint64_t max_level, float fraction) {
+    this->maximal_level = max_level;
+    this->fraction_of_nodes = fraction;
+    current_level = 0;
+    header = new Node(-1, max_level);
+}
+
+int SkipList::randomLevel() {
+    float probabillity_of_edge = static_cast<float>(rand()) / RAND_MAX;
+    uint64_t level = 0;
+    while (probabillity_of_edge < fraction_of_nodes && level < maximal_level) {
+        level++;
+        probabillity_of_edge = static_cast<float>(rand()) / RAND_MAX;
+    }
+    return level;
+}
+
--- source/skiplist.hpp
+++ source/skiplist.hpp
@@ -1,10 +1,27 @@
-//
-// Created by Shamil on 2019-11-19.
-//
-
 #ifndef SKIPLIST_SKIPLIST_H
 #define SKIPLIST_SKIPLIST_H
 
 #include <atomic>
+#include <cstring>
+#include <cstdlib>
+
+class Node {
+public:
+    int key;
+    Node **forward;
+
+    Node(int, uint64_t);
+};
+
+class SkipList {
+    uint64_t maximal_level;
+    uint64_t current_level;
+    float fraction_of_nodes;
+    Node *header;
+
+public:
+    SkipList(uint64_t, float);
+    int randomLevel(); //function to count how many time flipped coin lands of edge(creating new level for element)
+};
 
 #endif
